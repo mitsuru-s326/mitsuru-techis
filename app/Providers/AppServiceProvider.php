7db29bfd3a4dc8fe4fefe8f5fdc8441dce39d734
mitsuru-s326 <mitsuru5110@gmail.com>
2023-07-14 21:50:23 +0900

    202307145

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,13 +26,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         
-        //
-        // if (\App::environment(['production'])) {
-        //     \URL::forceScheme('https');
-        //     $this->app['request']->server->set('HTTPS','on');
+        
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
 
        
-        // }
+        }
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
