d441c845f93a171cd088f3a9b8f7db2f2ac3f2d0
mitsuru-s326 <mitsuru5110@gmail.com>
2023-07-18 20:43:30 +0900

    20230718

--- resources/views/search/detail.blade.php
+++ resources/views/search/detail.blade.php
@@ -3,6 +3,8 @@
     <title>料理の詳細</title>
 </head>
 
+<body>
+
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
      <a class="navbar-brand" href="/home">献立一覧へ</a>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
@@ -84,6 +86,14 @@
 </div>
 </div>
 
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+</body>
 <!-- <li><b>料理名：{{$item->title}}</b></li>
 <li>ジャンル：{{$item->genre}}</li>
 <li>料理時間：{{$item->time}}</li>
